9deb7fb1a314f824d2f71bf2b4a00902b4b0cc40
eapepe0 <eapepe0@gmail.com>
2021-11-13 04:47:04 -0300

    6to Ejercicio Finalizado

---
+++ app.js
@@ -0,0 +1,16 @@
+const page= (
+<div>
+    <a href="http://react.org"><img src="./react-logo.png" width="40px" alt="Logo react"></img></a>
+    <h1>Fun facts about React</h1>
+
+    <ul>
+        <li>Fue lanzado en 2013</li>
+        <li>Fue originalmente creado por Jordan Walke</li>
+        <li>Tiene mas de 100k de estrellas en Github</li>
+        <li>Es mantenido por Facebook</li>
+        <li>Enpodera cientos de apps de empresas, incluyendo aplicaciones mobiles</li>
+    </ul>
+</div>
+)
+
+ReactDOM.render(page , document.getElementById("root"))
